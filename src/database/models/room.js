62390bfa7cb22817bd7e9cf7d2e178438f77d81c
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-03-30 15:26:20 -0300

    Se agrego: el schema de las habitaciones en el archivo romm.js dentro de la carpeta models

---
+++ src/database/models/room.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+
+const roomSchema = new Schema({
+  numero: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 30,
+    unique: true,
+  },
+  tipoHabitacion: {
+    type: String,
+    required: true,
+    enum: ["Individual", "Doble", "Triple"],
+  },
+  categoria: {
+    type: String,
+    required: true,
+    enum: [
+      "Standard",
+      "Deluxe",
+      "Ejecutiva",
+      "Suite",
+      "Presidencial",
+      "Penthouse Suite",
+    ],
+  },
+  precio: {
+    type: Number,
+    required: true,
+    min: 5000,
+    max: 500000,
+  },
+  imagen: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (valor) => {
+        const pattern =
+          /(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))/i;
+        return pattern.test(valor);
+      },
+      message: (props) => `${props.value} no es una url válida`,
+    },
+  },
+  disponibilidad: {
+    type: Date,
+    required: true,
+  },
+});
