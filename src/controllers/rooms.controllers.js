951e0d2acdcce2d13ab881d37b63f371cc2756e7
lucianocabelloo <you@example.com>
2024-03-31 15:45:10 -0300

    Se agrego: funcion getRoomById en rooms.controllers

--- src/controllers/rooms.controllers.js
+++ src/controllers/rooms.controllers.js
@@ -12,3 +12,15 @@ export const getRooms = async (res, req) => {
     }
 }
 
+export const getRoomById =  async ( res, req) => {
+    try {
+        const id = req.params.id
+        const searchRoom = await Room.findById(id)
+        res.status(200).json(searchRoom)
+    } catch (error) {
+        console.error("Ha habido un error al buscar el cuarto:", error)
+        res.status(404).json({
+            mensaje: "No se ha encontrado la habitacion esa id"
+        })
+    }
+}
