638ec22ebc02d3f7581ab0269f2387b58b5cd966
nicoIosa <nicolasiosa@hotmail.com>
2024-04-01 19:32:17 -0300

    Se agregó: funcion deleteRoom en rooms.controllers

---
+++ src/controllers/rooms.controllers.js
@@ -0,0 +1,24 @@
+import Room from "../database/models/room.js";
+
+export const deleteRoom = async (res, req) => {
+  try {
+    const id = req.params.id;
+    const searchedRoom = await Room.findById(id);
+    if (!searchedRoom) {
+      return res.status(404).json({
+        message: "No se encontro la habitacion buscada",
+      });
+    }
+    await Room.findByIdAndDelete(id);
+    res
+      .status(200)
+      .json({ message: "La habitacion fue borrada correctamente" });
+  } catch (error) {
+    console.error(
+      `El siguiente error ocurrio al buscar la habitacion: ${error}`
+    );
+    res.status(500).json({
+      message: "Ocurrio un error al borrar la habitacion",
+    });
+  }
+};
