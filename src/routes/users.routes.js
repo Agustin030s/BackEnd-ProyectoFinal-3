66ca63c7f570fa5c1bad2905ed10652c8f6e2d58
Martín Cristian Cornejo <martincckd130@gmail.com>
2024-04-02 20:29:01 -0300

    Se agrego la ruta del obtenerUsuarios dentro del archivo users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { crearUsuario } from "../controllers/user.controllers.js";
-
+import { crearUsuario, obtenerUsuarios } from "../controllers/user.controllers.js";
 const router = Router();
 
 router.route('/nuevo').post(crearUsuario);
+router.route('/').get(obtenerUsuarios)
 
 export default router;
