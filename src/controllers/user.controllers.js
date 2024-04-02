6ce15c8fc96c3edd2948cb53265a75787faf368c
nicoIosa <nicolasiosa@hotmail.com>
2024-04-02 19:39:40 -0300

    se agregó: la funcion getUserByEmail en user.controllers

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,30 +1,42 @@
 import User from "../database/models/user.js";
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 
-export const crearUsuario = async (req,res) => {
-    try {
-        const {email, password} = req.body;
-        const usuarioBuscado = await User.findOne({email});
-
-        if (usuarioBuscado) {
-            return res.status(400).json({
-                message: "Ya existe un usuario con el email enviado"
-            })
-        }
-        const usuarioNuevo = new User(req.body);
-        // Encriptar la contraseña 
-        const salt = bcrypt.genSaltSync(10);
-        usuarioNuevo.password = bcrypt.hashSync(password,salt)
-
-        await usuarioNuevo.save();
-        res.status(201).json({
-            message: "Usuario dado de alta exitosamente"
-        })
-
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({
-            message: "El usuario no pudo ser dado de alta"
-        })
+export const crearUsuario = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const usuarioBuscado = await User.findOne({ email });
+    if (usuarioBuscado) {
+      return res.status(400).json({
+        message: "Ya existe un usuario con el email enviado",
+      });
     }
+    const usuarioNuevo = new User(req.body);
+    // Encriptar la contraseña
+    const salt = bcrypt.genSaltSync(10);
+    usuarioNuevo.password = bcrypt.hashSync(password, salt);
+    await usuarioNuevo.save();
+    res.status(201).json({
+      message: "Usuario dado de alta exitosamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      message: "El usuario no pudo ser dado de alta",
+    });
+  }
+};
+
+export const getUserByEmail = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const searchedUser = await User.findOne({ email });
+    res.status(200).json(searchedUser);
+  } catch (error) {
+    console.error(
+      `Los siguientes errores ocurrieron al intentar buscar el usuario: ${error}`
+    );
+    res.status(404).json({
+      message: "No se encontró el usuario con ese email",
+    });
+  }
 };
