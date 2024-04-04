0bd4e491d4f3ef324174705acfb03b6a24adc7ad
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-04-04 10:23:28 -0300

    Se agrego: las validaciones de JWT en las solicitudes post y put de las habitaciones y de los usuarios

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,13 +1,21 @@
 import { Router } from "express";
-import { crearUsuario, obtenerUsuarios, getUserByEmail, deleteUser, editUser } from "../controllers/user.controllers.js";
+import {
+  createUser,
+  obtenerUsuarios,
+  getUserByEmail,
+  deleteUser,
+  editUser,
+  login,
+} from "../controllers/user.controllers.js";
 import userValidation from "../helpers/userValidation.js";
+import JWTValidation from "../helpers/jwtValidation.js";
 
 const router = Router();
 
-
-router.route('/nuevo').post([userValidation], createUser);
-router.route('/nuevo/:id').put(editUser).delete(deleteUser)
-router.route("/").get(getUserByEmail, obtenerUsuarios);
-
+router.route("/").get(obtenerUsuarios).post(login);
+router.route("/nuevo").post([userValidation], createUser);
+router.route("/actualizar/:id").put([JWTValidation, userValidation], editUser);
+router.route("/eliminar/:id").delete(deleteUser);
+router.route("/obtenerEmail").get(getUserByEmail);
 
 export default router;
