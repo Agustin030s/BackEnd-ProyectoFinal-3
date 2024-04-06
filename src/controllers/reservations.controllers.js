54d0e0d1a0076951625a8f42c7b7d4cc81e7505d
nicoIosa <nicolasiosa@hotmail.com>
2024-04-06 18:19:27 -0300

    Se agregó: la funcion deleteReservation en reservations.controllers

--- src/controllers/reservations.controllers.js
+++ src/controllers/reservations.controllers.js
@@ -4,48 +4,38 @@ import Room from "../database/models/room.js";
 export const reserveRoom = async (req, res) => {
   try {
     const { numHabitacion, fechaInicio, fechaFin } = req.body;
-
     const room = await Room.findOne({ numero: numHabitacion });
-
     if (!room)
       return res.status(404).json({ mensaje: "Habitación no encontrada" });
-
     if (!room.disponibilidad)
       return res
         .status(400)
         .json({ mensaje: "La habitación no esta disponible" });
-
     const currentDate = new Date();
     const initReserve = new Date(fechaInicio);
     const finishReserve = new Date(fechaFin);
-
     if (initReserve < currentDate)
       return res.status(400).json({
         mensaje:
           "La fecha de inicio de la reserva no puede ser menor a la fecha actual",
       });
-
     if (finishReserve <= initReserve)
       return res.status(400).json({
         mensaje:
           "La fecha de fin no puede ser menor o igual a la fecha de inicio",
       });
-
     // Verificar si hay reservas que se solapen con las fechas solicitadas
     const overlappingReservation = await Reservation.findOne({
       numHabitacion,
       fechaInicio: { $lte: fechaFin },
       fechaFin: { $gte: fechaInicio },
     });
-
     if (overlappingReservation) {
       return res.status(400).json({
         mensaje: "Ya existe una reserva en las fechas seleccionadas",
       });
     }
-
     const newReservation = new Reservation(req.body);
-
     await newReservation.save();
     res.status(201).json({
       mensaje: "Reserva creada con éxito",
@@ -62,17 +52,36 @@ export const getReservationForARoom = async (req, res) => {
   try {
     const numero = req.params.numero;
     const reservations = await Reservation.find({ numHabitacion: numero });
-
     if (reservations.length === 0)
       return res
         .status(200)
         .json({ mensaje: "La habitación no tiene reservas" });
-
     res.status(200).json(reservations);
   } catch (error) {
     console.error(error);
     res.status(404).json({
       mensaje: "No se pudo obtener las reservas",
+    });
+  }
+};
+
+export const deleteReservation = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const searchedReservation = await Reservation.findById(id);
+    if (!searchedReservation) {
+      return res.status(404).json({
+        message: "No se encontro la reserva buscada",
+      });
+    }
+    await Reservation.findByIdAndDelete(id);
+    res.status(200).json({ message: "La reserva fue borrada correctamente" });
+  } catch (error) {
+    console.error(
+      `El siguiente error ocurrio al intentar borrar la reserva: ${error}`
+    );
+    res.status(500).json({
+      message: "Ocurrio un error al borrar la reserva",
     });
   }
 };
