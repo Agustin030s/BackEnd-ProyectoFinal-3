93e0d94d40390312341ecf52f4b4f4c0977d27fa
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-04-06 11:51:22 -0300

    Se agrego: El modelo de las reservas

---
+++ src/database/models/reservation.js
@@ -0,0 +1,54 @@
+import mongoose, { Schema } from "mongoose";
+
+const reservationSchema = new Schema({
+  nombreCompleto: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 80,
+  },
+  dni: {
+    type: Number,
+    required: true,
+    min: 8,
+    max: 10,
+    unique: true,
+  },
+  telefono: {
+    type: Number,
+    required: true,
+    min: 9,
+    max: 15,
+  },
+  email: {
+    type: String,
+    required: true,
+    minLength: 10,
+    maxLength: 340,
+    validate: {
+      validator: (valor) => {
+        const pattern =
+          /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        return pattern.test(valor);
+      },
+      message: (props) => `${props.value} no es un email válido`,
+    },
+    unique: true,
+  },
+  fechaInicio: {
+    type: Date,
+    required: true,
+  },
+  fechaFin: {
+    type: Date,
+    required: true,
+  },
+  numHabitacion: {
+    type: Schema.ObjectId,
+    ref: "room",
+  },
+});
+
+const Reservation = mongoose.model("reservation", reservationSchema);
+
+export default Reservation;
