1f0159544292389679407857044f9406565a47d9
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-04-14 14:54:40 -0300

    Se modifico: las validaciones de las reservas para validar correctamente las fechas

--- src/helpers/reservationValidation.js
+++ src/helpers/reservationValidation.js
@@ -34,42 +34,19 @@ const reservationValidation = [
     .notEmpty()
     .withMessage("El numero de habitacion es un dato obligatorio")
     .isNumeric()
-    .withMessage("El numero de habitacion tiene que ser un valor numerico")
-    .custom(async (value) => {
-      const existingRoom = await Room.findOne({ numero: value });
-      if (existingRoom) {
-        throw new Error("El número de habitación ya está en uso");
-      }
-      return true;
-    })
-    .custom((value) => {
-      if (value >= 1 && value <= 30) {
-        return true;
-      } else {
-        throw new Error("El numero de habitacion debe estar entre 1 y 30");
-      }
-    }),
+    .withMessage("El numero de habitacion tiene que ser un valor numerico"),
   check("fechaInicio")
     .notEmpty()
     .withMessage("La fecha de inicio es un dato obligatorio")
-    .isDate()
+    .isISO8601()
+    .toDate()
     .withMessage("La feche de inicio debe ser formato Date"),
   check("fechaFin")
     .notEmpty()
     .withMessage("La fecha de fin es un dato obligatorio")
-    .isDate()
+    .isISO8601()
+    .toDate()
     .withMessage("La feche de fin debe ser formato Date"),
-  check("total")
-    .notEmpty()
-    .withMessage("El total es obligatorio")
-    .custom((value) => {
-      if (value >= 4000 && value <= 20000000) {
-        return true;
-      } else {
-        throw new Error("El total debe ser entre 4 mil y 20 millones");
-      }
-    })
-    .withMessage("El total debe ser entre 4 mil y 20 millones"),
   (req, res, next) => validationResultFunction(req, res, next),
 ];
 
